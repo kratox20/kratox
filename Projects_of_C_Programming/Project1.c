e993c436b916e80587b77b118e778f7a6cf213d9
yaswebdev <yassinerami.official@gmail.com>
2023-08-28 16:24:46 +0100

    edit file

--- Projects_of_C_Programming/Project1.c
+++ Projects_of_C_Programming/Project1.c
@@ -4,13 +4,21 @@
  * Return: Always 0 (success)
  */
 int main(){
-int T[6];
+int T[10];
+int min;
 int *P;
 
 P = T;
 printf("enter the value of all elements in the table : \n");
-for ( P = T ; P < T + 6 ; P++ ){
+for ( P = T ; P < T + 10 ; P++ ){
 printf("T[%ld] = " , P-T);
 scanf("%d",P);
 }
+min = *T;
+for ( P = T + 1 ; P < T + 10 ; P++ ){
+	if(min>*P)
+		min = *P;
+}
+printf("the minimum is : %d \n", min);
+return 0 ;
 }
