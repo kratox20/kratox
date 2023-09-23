1831dd4de486f151152f4dd2942c7437a3433a83
kratox20 <yassino.ramiro@gmail.com>
2023-09-23 02:00:48 +0100

    ffjf

--- JavaScript_Courses/array.filter.js
+++ JavaScript_Courses/array.filter.js
@@ -1,12 +1,12 @@
 let ages = [18 ,20 ,50 ,17 ,13 ,90];
 let adults = ages.filter(checkage);
 
-adults.forEach(print);
+adults.forEach(prin);
 
 function checkage(element){
 	    return element >= 18 ;
 }
 
-function print(element){
+function prin(element){
 	    console.log(element);
 }
