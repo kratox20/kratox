c76f0240b4dc0c130c2380184b796e58d4d67086
kratox20 <yassino.ramiro@gmail.com>
2024-02-11 14:41:11 +0100

    add python file of string and slicing named string

---
+++ python_app/string_slicing.py
@@ -0,0 +1,13 @@
+name = "yassine rami"
+
+first_name = name[:3]
+last_name = name[4:]
+funky_name = name[0:7:2]
+reversed_name = name[::-1]
+
+print(reversed_name)
+
+website1 = "http://youcan.ma"
+
+name_of_site = slice(7,-3)
+print(website1[name_of_site]) #-----> output: youcan
