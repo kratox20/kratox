3db61249897b2c6bf17023b3deadb84dacaa9ea3
kratox20 <yassino.ramiro@gmail.com>
2024-02-11 13:56:21 +0100

    add math functions

---
+++ python_app/main.py
@@ -0,0 +1,5 @@
+name = input("enter your name : ")
+age = int(input("how old are you : "))
+
+print("your name is : "+ name)
+print("you are "+ str(age) +" years old !")
---
+++ python_app/math_functions_python.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+
+x = 1
+y = 2
+z = 3
+
+print(round(pi)) #---------> output: 3
+print(math.ceil(pi)) #---------> output: 4
+print(math.floor(pi)) #---------> output: 3
+print(abs(pi)) #---------> if pi = -3.14 ------> output : 3.14
+print(pow(pi,2)) #---------> output: 3.14 * 3.14 = 9.8596
+print(math.sqrt(420)) #-----> output: 20.49........etc
+print(max(x,y,z)) #--------> output: 3
+print(min(x,y,z)) #-----> output: 1
