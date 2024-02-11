f26070da7ba02fc1debbc2dbcd9b120cec133503
kratox20 <yassino.ramiro@gmail.com>
2024-02-11 14:56:51 +0100

    add file of python named if_statements

---
+++ python_app/if_statements.py
@@ -0,0 +1,10 @@
+
+age = int(input("how old are you : "))
+
+
+if age == 21:
+    print("you are a young man")
+elif age > 21:
+    print("you are a true man")
+else:
+    print("you are a child !")
